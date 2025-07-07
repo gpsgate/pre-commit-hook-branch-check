7b30d8932144994139fb41de8bc76b49eebaa463
Emmanuel Frécon <emmanuel.frecon@gpsgate.com>
2025-07-07 15:44:17 +0200

    🐛 fix: main allowed by CC

--- pre_commit_hooks/branch_check.py
+++ pre_commit_hooks/branch_check.py
@@ -8,11 +8,10 @@ import re
 # Default patterns are for conventional commits. See:
 # https://conventional-branch.github.io/
 DEFAULT_ALLOWED_PATTERNS = [
-    '^(feature|bugfix|hotfix|release|chore)/[a-z0-9/.-]+$'
+    '^(feature|bugfix|hotfix|release|chore)/[a-z0-9/.-]*[a-z0-9]$',
+    '^(main|master|develop)$'
 ]
-DEFAULT_DENIED_PATTERNS = [
-    '^(main|master)$'
-]
+DEFAULT_DENIED_PATTERNS = []
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -48,15 +47,23 @@ def main(argv: Sequence[str] | None = None) -> int:
         help='Regular expression that branch name should not match, can be repeated.'
     )
 
+    # Parse the command-line arguments and pick good defaults
     args = parser.parse_args(argv)
     allow_patterns = args.allow or DEFAULT_ALLOWED_PATTERNS
     deny_patterns = args.deny or DEFAULT_DENIED_PATTERNS
-    branch_name = subprocess.check_output(
-        ['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('utf-8').strip()
-    if not branch_name:
-        print('No branch name found. Are you in a git repository?')
+
+    # Detect the current branch name using git
+    try:
+        branch_name = subprocess.check_output(
+            ['git', 'rev-parse', '--abbrev-ref', 'HEAD'])\
+            .decode('utf-8').strip()
+    except FileNotFoundError:
+        print('Error: git not installed or not found in PATH.')
         return 1
-    
+    except subprocess.CalledProcessError:
+        print('Error: failed to get current branch name. Are you in a git repository?')
+        return 1
+
     # Check if the branch name matches any of the allowed patterns
     if not any(re.match(pattern, branch_name) for pattern in allow_patterns):
         print(f'Branch name "{branch_name}" does not match any of the allowed patterns: {allow_patterns}')
@@ -67,7 +74,7 @@ def main(argv: Sequence[str] | None = None) -> int:
         return 1
     print(f'Branch name "{branch_name}" is valid.')
     return 0
-    
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
